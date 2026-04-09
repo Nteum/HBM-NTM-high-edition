17e30da09d7b4da7b73786480bb407122b8ad3e6
Basyaact <2095657527@qq.com>
2026-04-09 13:15:00 +0800

    修正package-patch

--- src/main/java/com/hbm/render/model/ModelQC.java
+++ src/main/java/com/hbm/render/model/ModelQC.java
@@ -1,4 +1,4 @@
-//在客户端启动时做一次离线质检，把常见问题直接打到日志
+//在客户端启动时做一次离线质检，把常见问题直接打到日志.
 package com.hbm.render.model; //没注意package默认的，导致无法PortQC
 
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
